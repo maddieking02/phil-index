89b907021b953baa4518ccd7620e50a5fd3d16c5
Madeline King <madeline.king330@gmail.com>
2023-05-23 21:52:46 -0700

    setup react-redux boilerplate

---
+++ client/src/reducer.ts
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState: {
+
+} = {
+
+};
+
+export const Slice = createSlice({
+  name: '',
+  initialState,
+  reducers: {
+  },
+});
+
+export const { } = Slice.actions;
+export default Slice.reducer;
